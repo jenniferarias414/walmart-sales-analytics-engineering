ce770cd6c64e72b33072c073175a123787a5b4bf
Jenny Arias <jenniferarias414@gmail.com>
2026-07-26 12:06:45 -0500

    walmart_department_sales model done

--- dbt/walmart_sales_analytics/models/staging/stg_walmart_department_sales.sql
+++ dbt/walmart_sales_analytics/models/staging/stg_walmart_department_sales.sql
@@ -1,3 +1,4 @@
+cat > models/staging/stg_walmart_department_sales.sql <<'SQL'
 with source as (
 
     select *
@@ -7,9 +8,20 @@ with source as (
 
 renamed_and_casted as (
 
-   TODO: Add column renaming and type casting here, if needed
+    select
+        try_cast(store_raw as number(10, 0)) as store_id,
+        try_cast(dept_raw as number(10, 0)) as dept_id,
+        try_to_date(date_raw) as store_date,
+        try_cast(weekly_sales_raw as number(18, 2)) as store_weekly_sales,
+        try_cast(is_holiday_raw as boolean) as is_holiday,
+
+        source_file_name,
+        loaded_at
+
+    from source
 
 )
 
 select *
 from renamed_and_casted
+SQL
